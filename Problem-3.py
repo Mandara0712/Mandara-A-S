e1fe632a95711d7914af48d9ed3345a423e61adb
Mandara A S <mandarashivakumar1773@gmail.com>
2025-12-18 19:16:28 +0530

    Add script to print odd numbers up to user input
    This script takes a user input number and prints all odd numbers up to that number, adjusting the input if it's even.

---
+++ Problem-3.py
@@ -0,0 +1,10 @@
+# Take input from user
+a = int(input("Enter a number: "))
+
+
+if a % 2 == 0:
+    a = a - 1
+
+# Generate and print odd numbers
+for i in range(1, a + 1):
+    print(2 * i - 1, end=" ")
