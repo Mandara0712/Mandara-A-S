0c4d82dc11a6c1f5b962114e1071ad63017f40d2
Mandara A S <mandarashivakumar1773@gmail.com>
2025-12-18 19:17:44 +0530

    Implement divisor counting for numbers 1-9
    The script counts how many numbers from a predefined list are divisible by each integer from 1 to 9 and stores the results in a dictionary.

---
+++ Problem-4.py
@@ -0,0 +1,13 @@
+numbers = [1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30]
+
+# Dictionary to store result
+result = {}
+for i in range(1, 10):
+    count = 0
+    for num in numbers:
+        if num % i == 0:
+            count += 1
+    result[i] = count
+
+# Print the output dictionary
+print(result)
