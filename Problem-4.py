37430dc882c2d67fa2fff0f59181df386393118c
Mandara A S <mandarashivakumar1773@gmail.com>
2025-12-18 19:24:10 +0530

    Add initial setup for counting multiples in a list

--- Problem-4.py
+++ Problem-4.py
@@ -1,7 +1,10 @@
+# Given input list
 numbers = [1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30]
 
 # Dictionary to store result
 result = {}
+
+# Check multiples from 1 to 9
 for i in range(1, 10):
     count = 0
     for num in numbers:
